a9f95d9c2ab5d9a6340bd24ddad377d9f94dd5ed
rajukurudi <rajukurudi@gmail.com>
2017-12-20 14:55:55 +0530

    solve

--- DevOpsDemo1/src/Demo.java
+++ DevOpsDemo1/src/Demo.java
@@ -4,8 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("Hello Banagalore");
 	}
 
 }
